6d24f4185aaeba25b90630ce27c811859f82c5ab
liwenkai <2020583117@qq.com>
2024-03-16 23:05:49 +0800

    fix: 简单调整

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { format, parseISO } from 'date-fns'
-import { allPosts } from 'contentlayer/generated'
+import { allPosts, type Post } from 'contentlayer/generated'
 
 export async function getStaticPaths() {
   const paths = allPosts.map((post) => post.url)
@@ -12,29 +12,27 @@ export async function getStaticPaths() {
 }
 
 const PostLayout = ({ params }: { params: { slug: string } }) => {
-  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug) as Post
 
-    return (
-      <>
-        <Head>
-          <title>{post.title}</title>
-        </Head>
-        <article className="mx-auto max-w-2xl py-16">
-          <div className="mb-6 text-center">
-            <Link href="/">
-              {/* <a className="text-center text-sm font-bold uppercase text-blue-700">Home</a> */}
-            </Link>
-          </div>
-          <div className="mb-6 text-center">
-            <h1 className="mb-1 text-3xl font-bold">{post.title}</h1>
-            <time dateTime={post.date} className="text-sm text-slate-600">
-              {format(parseISO(post.date), 'LLLL d, yyyy')}
-            </time>
-          </div>
-          <div className="cl-post-body" dangerouslySetInnerHTML={{ __html: post.body.html }} />
-        </article>
-      </>
-    )
+  return (
+    <>
+      <Head>
+        <title>{post?.title}</title>
+      </Head>
+      <article className="mx-auto max-w-2xl py-16">
+        <div className="mb-6 text-center">
+          <Link href="/"></Link>
+        </div>
+        <div className="mb-6 text-center">
+          <h1 className="mb-1 text-3xl font-bold">{post?.title}</h1>
+          <time dateTime={post?.date} className="text-sm text-slate-600">
+            {format(parseISO(post?.date), 'LLLL d, yyyy')}
+          </time>
+        </div>
+        <div className="cl-post-body" dangerouslySetInnerHTML={{ __html: post?.body.html }} />
+      </article>
+    </>
+  )
 }
 
 export default PostLayout
